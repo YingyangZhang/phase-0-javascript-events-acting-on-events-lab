c5c93cb999049afb7f78a36fc78e09d9a0313637
Yingyang Zhang <yingyang.zhang95@gmail.com>
2022-08-31 17:00:02 -0400

    commit tree 1e5fa7a6bdc43256e317473be7ab6b6db29adf5c

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Your code here
+
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft(){
+    const leftNumber = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumber, 10);
+
+    if(left > 0){
+        dodger.style.left = `${left - 10}px`;
+    }
+}
+
+function moveDodgerRight(){
+    const rightNumber = dodger.style.left.replace("px", "");
+    const right = parseInt(rightNumber, 10);
+
+    if(right < 360){
+        dodger.style.left = `${right+ 10}px`;
+    }
+}
+
+document.addEventListener("keydown", e => {
+    switch(e.key){
+        case ("ArrowLeft"):
+            moveDodgerLeft();
+            break;
+        case ("ArrowRight"):
+            moveDodgerRight();
+            break;
+    }
+});
